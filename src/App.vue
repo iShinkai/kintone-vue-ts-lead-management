fc432a4bfa9055087503983b6caaebf4daf089a5
新貝博幸 <shinkai@clare.local>
2019-12-01 11:08:21 +0900

    [update] Pug プラグイン追加、App.vue を Pug でリライト

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+  #app
+    img(alt="Vue logo" src="./assets/logo.png")
+    HelloWorld(msg="はじめての Vue.js + TypeScript App です！")
 </template>
 
 <script lang="ts">
